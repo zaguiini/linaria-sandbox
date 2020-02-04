6f72c781b97c888ab2e730313d41d6fedd43f848
Luis Felipe Zaguini <26530524+zaguiini@users.noreply.github.com>
2020-02-03 23:51:05 -0300

    starting structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import { styled } from 'linaria/lib/react'
+import { media } from 'styles/utils'
 
 export const Title = styled.div`
   background: blue;
+
+  ${media.medium} {
+    background: yellow;
+  }
+
+  ${media.large} {
+    background: green;
+  }
+
+  ${media.extraLarge} {
+    background: pink;
+  }
 `
 
-const App = () => <Title>Grande</Title>
+const App = () => <Title>Grande diazao</Title>
 
 export default App
---
+++ src/styles/utils.ts
@@ -0,0 +1,5 @@
+export const media = {
+  medium: '@media (min-width: 768px)',
+  large: '@media (min-width: 991px)',
+  extraLarge: '@media (min-width: 1200px)',
+}
